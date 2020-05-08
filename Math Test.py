1c42e62ee4a9d80dab2e5fa5d2bed33d7e2b5bc6
Austin King <aking8@umbc.edu>
2020-05-08 15:59:35 -0400

    got the scraping, lat and long collection, and then the distance calculation working as well

--- Math Test.py
+++ Math Test.py
@@ -4,19 +4,24 @@ if __name__ == '__main__':
     bridge_distances = []
     R = 6373.0
     # Radius of the Earth
-    base_location = [50, 50, "Doesn't matter", "name"]
-    base_lat = base_location[0]
-    base_long = base_location[1]
-    # user location
-    new_location = [30, 30, "Doesn't matter", "name"]
-    new_lat = radians(new_location[0])
-    new_long = radians(new_location[1])
+
+    user_location = [50, 50, "Doesn't matter", "name"]
+    user_lat = user_location[0]
+    user_long = user_location[1]
+    # user location information
+
+    bridge_location = [30, 30, "Doesn't matter", "name"]
+    bridge_lat = radians(bridge_location[0])
+    bridge_long = radians(bridge_location[1])
     # bridge location
-    distance_lat = new_lat - base_lat
-    distance_long = new_long - base_long
-    a = sin(distance_lat/2)**2 + cos(base_lat)*cos(new_lat)*sin(distance_long/2)**2
+
+    distance_lat = bridge_lat - user_lat
+    distance_long = bridge_long - user_long
+    a = sin(distance_lat/2)**2 + cos(user_lat)*cos(bridge_lat)*sin(distance_long/2)**2
     c = 2* atan2(sqrt(a), sqrt(1-a))
+
     distance = float(R*c)
-    bridge_distances.append({new_location[3]: distance})
+    bridge_distances.append({bridge_location[3]: distance})
     # calculates distance of bridges from user
+
     print(bridge_distances)
--- TestGetBridgeInfo.py
+++ TestGetBridgeInfo.py
@@ -1,16 +1,50 @@
 import requests
+from math import sin, cos, sqrt, atan2, radians
 from bs4 import BeautifulSoup
+
+USER_LOCATION = [50, 50, 'some', 'AUSTINTEST']
+# test user location data
+
+
+def distance_calc(base_location, new_location):
+    R = 6373.0
+    # Radius of the Earth
+    popularity_num = new_location[2]
+    # daily traffic numbers
+
+    user_location = base_location
+    user_lat = user_location[0]
+    user_long = user_location[1]
+    # user location information
+
+    bridge_location = new_location
+    bridge_lat = radians(bridge_location[0])
+    bridge_long = radians(bridge_location[1])
+    # bridge location
+
+    distance_lat = bridge_lat - user_lat
+    distance_long = bridge_long - user_long
+    a = sin(distance_lat / 2) ** 2 + cos(user_lat) * cos(bridge_lat) * sin(distance_long / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance_to_user = float(R * c)
+    return [bridge_location[3], distance_to_user, popularity_num]
+    # calculates distance of bridges from user
+
 
 if __name__ == '__main__':
     bridge_start_url = 'https://bridgehunter.com/category/status/open/'
     # keeps url
+
     bridge_content = requests.get(bridge_start_url).text
     # get request the HTML content
+
     soup = BeautifulSoup(bridge_content, 'html.parser')
     # parses through the html page text
+
     link_bridges = soup.select('div.x a.name')
-    print(link_bridges)
     # finds all bridge names and url pairs for each bridge
+
     page = 1
     bridge_list = []
     bridge = 0
@@ -22,7 +56,6 @@ if __name__ == '__main__':
         bridge += 1
         # makes list with bridge name and it's url
     # bridge_list is filled with all the bridges on this page and their urls
-    print(bridge_list[0][1])
 
     """while page <= 228:
         bridge_page_url = 'https://bridgehunter.com/category/status/open/page' + str(page) + '/'
@@ -33,6 +66,7 @@ if __name__ == '__main__':
         link_bridges = soup.select('div.x a.name')
         print('Made it through a page')
         # finds all bridge names and url pairs for each bridge
+        
         bridge = 0
         while bridge < len(link_bridges):
             href = link_bridges[bridge].get('href')
@@ -42,11 +76,13 @@ if __name__ == '__main__':
             bridge += 1
             # makes list with bridge name and it's url
         # bridge_list is filled with all the bridges on this page and their urls
+        
         print("another bridge down")
         page += 1
     print('We done did it!')"""
     # gets the name and link for every bridge
     bridge_info = 0
+    bridge_info_list = []
     while bridge_info < len(bridge_list):
         bridge_page = bridge_list[bridge_info][1]
         bridge_url = 'https://bridgehunter.com' + bridge_page
@@ -64,8 +100,10 @@ if __name__ == '__main__':
             bridge_latitude_longitude_traffic_name.append(bridge_longitude)
             bridge_latitude_longitude_traffic_name.append(bridge_traffic)
             bridge_latitude_longitude_traffic_name.append(bridge_list[bridge_info][0])
-            print(bridge_latitude_longitude_traffic_name)
+            print(bridge_info_list)
             # latitude, longitude, and traffic made into a list
+            bridge_latitude_longitude_traffic_name.append(bridge_latitude_longitude_traffic_name)
+            bridge_info_list.append(distance_calc(USER_LOCATION, bridge_latitude_longitude_traffic_name))
             bridge_info += 1
 
         except:
@@ -74,6 +112,16 @@ if __name__ == '__main__':
             bridge_latitude_longitude_traffic_name.append(bridge_longitude)
             bridge_latitude_longitude_traffic_name.append(bridge_traffic)
             bridge_latitude_longitude_traffic_name.append(bridge_list[bridge_info][0])
-            print(bridge_latitude_longitude_traffic_name)
-            # latitude, longitude, and traffic made into a list
+            print(bridge_info_list)
+            # latitude, longitude, traffic, and name made into a list
+            bridge_latitude_longitude_traffic_name.append(bridge_latitude_longitude_traffic_name)
+            distance = distance_calc(USER_LOCATION, bridge_latitude_longitude_traffic_name)
+            bridge_info_list.append(distance)
             bridge_info += 1
+
+    bridge_info_list.sort(key=lambda bridge_info_list: bridge_info_list[1])
+    print(bridge_info_list)
+    top_five_closest_bridges = bridge_info_list[:5]
+    print(top_five_closest_bridges)
+    top_five_closest_bridges.sort(key=lambda top_five_closest_bridges: top_five_closest_bridges[2], reverse=True)
+    print(top_five_closest_bridges)
