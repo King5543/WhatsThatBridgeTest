cf4362b6a8565c45fc54fd4fa47bab5c33a0f189
Austin King <aking8@umbc.edu>
2020-05-20 18:31:51 -0400

    fixed the test so the data is correct now

--- Math Test.py
+++ Math Test.py
@@ -1,27 +1,46 @@
 from math import sin, cos, sqrt, atan2, radians
 
 if __name__ == '__main__':
-    bridge_distances = []
-    R = 6373.0
-    # Radius of the Earth
-
-    user_location = [50, 50, "Doesn't matter", "name"]
+    user_location = [lat, long]
     user_lat = user_location[0]
     user_long = user_location[1]
     # user location information
 
-    bridge_location = [30, 30, "Doesn't matter", "name"]
-    bridge_lat = radians(bridge_location[0])
-    bridge_long = radians(bridge_location[1])
-    # bridge location
+    with open('whatsthatbridgedata.csv') as f:
+        file_reader = csv.reader(f)
 
-    distance_lat = bridge_lat - user_lat
-    distance_long = bridge_long - user_long
-    a = sin(distance_lat/2)**2 + cos(user_lat)*cos(bridge_lat)*sin(distance_long/2)**2
-    c = 2* atan2(sqrt(a), sqrt(1-a))
+        ranking_list = []
+        for i in file_reader:
+            bridge_latitude = i[0]
+            bridge_longitude = i[1]
 
-    distance = float(R*c)
-    bridge_distances.append({bridge_location[3]: distance})
-    # calculates distance of bridges from user
+            R = 6373.0
+            # Radius of the Earth
+            popularity_num = float(i[2])
+            # daily traffic numbers
 
-    print(bridge_distances)
+            user_lat = USERLOCATION[0]
+            user_long = USERLOCATION[1]
+            # user location information
+
+            bridge_location = [bridge_latitude, bridge_longitude]
+            bridge_lat = radians(float(bridge_location[0]))
+            bridge_long = radians(float(bridge_location[1]))
+            # bridge location
+
+            distance_lat = bridge_lat - user_lat
+            distance_long = bridge_long - user_long
+            a = sin(distance_lat / 2) ** 2 + cos(user_lat) * cos(bridge_lat) * sin(distance_long / 2) ** 2
+            c = 2 * atan2(sqrt(a), sqrt(1 - a))
+            # calculates distance of bridges from user
+
+            distance_to_user = float(R * c)
+            ranking_info = [i[3], distance_to_user, popularity_num]
+            # returns bridge name, distance from user, and daily use number
+            ranking_list.append(ranking_info)
+
+    ranking_list.sort(key=lambda ranking_list: ranking_list[1])
+    f.close()
+    top_five_list = ranking_list[:5]
+    top_five_list.sort(key=lambda top_five_list: top_five_list[2])
+
